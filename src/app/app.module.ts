7b9e6737d11376aa2fdef0136b5fbc758d6d2b76
HikaruKondo <mushin.hudoushin@gmail.com>
2021-02-20 18:11:46 +0900

    No.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
-// import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    // HeroDetailComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
+ providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
